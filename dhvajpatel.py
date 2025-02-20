2d43b2ffffee3f37a1d720c84dfdcba18cfaf7d6
DELL <dhvajapatel.bscibm2024@iar.ac.in>
2025-02-20 16:24:43 +0530

    dhvajpatel.py

--- dhvajpatel.py
+++ dhvajpatel.py
@@ -1,5 +1,6 @@
-for x in range (1,11):
-    if x%2==0:
-        print(f"even number:{x}")
-    else:
-        print(f"odd number:{x}")
+### Find number is even or odd
+x = int(input("Enter a number: "))
+if x % 2 == 0:
+    print(f"even number:{x}")
+else:
+    print(f"odd number:{x}")
